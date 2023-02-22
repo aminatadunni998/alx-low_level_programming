04d0084012a746f3b1b499358c33a360c7ce9210
Awojobi omolade <aminatadunni998@gmail.com>
2023-02-22 10:13:31 +0000

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- largest_numbers - returns the largest of 3 numbers
+ * largest_numbers - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
