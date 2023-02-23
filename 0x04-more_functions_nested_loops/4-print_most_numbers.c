3170ba9b6ab05ea16d2ea1e8f063f4af8aaf72d7
Awojobi omolade <aminatadunni998@gmail.com>
2023-02-23 15:36:48 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,10 @@
 #include "main'h"
 
 /**
- * print_most_numbers - Print the numbers since 0 up to 9
- * Description: Prints the numbers excluding 2 and 4
- * Return: The numbers since 0 up to 0
+ * print_most_numbers - Prints
+ *
+ * Return: void
  */
-
 void print_most_numbers(void)
 {
 char c;
