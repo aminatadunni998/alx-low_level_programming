86a290731e087ed92a68823034ba0989bfc546b6
Awojobi omolade <aminatadunni998@gmail.com>
2023-02-23 15:32:25 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,11 @@
 
 void print_most_numbers(void)
 {
-int x = 0;
-for (; x <= 9; x++)
+char c;
+for (c = '0'; c <= '9'; c++)
 {
-if (x == 2 || x == 4)
-{
-continue;
-}
-else
-{
-_putchar(x + '0');
-}
+if (!(c == '2' || c == '4'))
+_putchar(c);
 }
 _putchar('\n');
 }
