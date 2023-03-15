f475e4b534bb292a94148337cae4e45eaa1f59fc
aminatadunni998 <aminatadunni998@gmail.com>
2023-03-15 05:53:08 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,20 +10,22 @@
 
 int _atoi(char *s)
 {
-
 int i, d, n, len, f, digit;
+
 i = 0;
 d = 0;
 n = 0;
 len = 0;
 f = 0;
 digit = 0;
+
 while (s[len] != '\0')
 len++;
 while (i < len && f == 0)
 {
-if (s[i] == '_')
+if (s[i] == '-')
 ++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
@@ -38,6 +40,7 @@ f = 0;
 i++;
 }
 if (f == 0)
+
 return (0);
 return (n);
 }
@@ -49,18 +52,21 @@ return (n);
  *
  * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
+
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
+
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1  *num2;
-
+result = num1 * num2;
 printf("%d\n", result);
+
 return (0);
 }
