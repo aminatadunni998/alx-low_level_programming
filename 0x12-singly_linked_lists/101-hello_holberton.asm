4b702d3bc439bfcccb8e00fa6a0c2e282c2e5f3a
aminatadunni998 <aminatadunni1998@gmail.com>
2023-03-31 08:15:16 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+message db "Hello, Holberton", 0x0A, 0x00
+
+section .text
+global main
+extern printf
+
+main:
+push rbp
+mov rbp, rsp
+
+lea rdi, [message]
+mov rax, 0
+call printf
+
+mov rsp, rbp
+pop rbp
+mov rax, 0
+ret
